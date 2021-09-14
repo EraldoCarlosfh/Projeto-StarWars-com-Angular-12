3c4c5221cf2ef578762a5bdb904d1082f9ab32d2
EraldoCarlosfh <79254386+EraldoCarlosfh@users.noreply.github.com>
2021-09-14 19:33:07 -0300

    Componentes criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { PilotsComponent } from './pilots/pilots.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HomeComponent,
+      StarshipsComponent,
+      PilotsComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
