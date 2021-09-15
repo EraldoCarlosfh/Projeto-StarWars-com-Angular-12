863c05283fcedec7f8671befed123a29a52154ee
EraldoCarlosfh <79254386+EraldoCarlosfh@users.noreply.github.com>
2021-09-15 02:49:03 -0300

    Adicionado fontawesome e bootstrap ao projeto e ajustado telas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PilotsComponent } from './pilots/pilots.component';
+import { StarshipsComponent } from './starships/starships.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'pilots', component: PilotsComponent},
+  { path: 'starships', component: StarshipsComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { StarshipsComponent } from './starships/starships.component';
 import { PilotsComponent } from './pilots/pilots.component';
 
 @NgModule({
-  declarations: [			
-    AppComponent,
+  declarations: [
+      AppComponent,
+      NavComponent,
+      FooterComponent,
       HomeComponent,
       StarshipsComponent,
       PilotsComponent
    ],
   imports: [
     BrowserModule,
+    CollapseModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
