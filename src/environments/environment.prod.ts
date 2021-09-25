111410945242057302097cf3dcc6057980fa84a6
EraldoCarlosfh <79254386+EraldoCarlosfh@users.noreply.github.com>
2021-09-25 03:07:02 -0300

    Ajustado environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  api: 'http://localhost:3000/', // starships
-  api2: 'http://localhost:3001/', // people
-  api3: 'http://localhost:3002/', // films
+  api: 'https://apirest-starwars.herokuapp.com/api/',
 };
